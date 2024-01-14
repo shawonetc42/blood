3cbc238e719e379a57e642583c50ed53c63450ca
shawonetc42 <139037447+shawonetc42@users.noreply.github.com>
2024-01-14 16:48:04 +0600

    Update Navber.js v.1

--- components/Navber/Navber.js
+++ components/Navber/Navber.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
+import { FaBars } from 'react-icons/fa';
 
 function Navbar() {
   const router = useRouter();
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   const NavLink = ({ href, children }) => {
     const isActive = router.pathname === href;
@@ -23,34 +29,38 @@ function Navbar() {
   };
 
   return (
-    <div className="flex justify-between items-center bg-white max-w-screen-md mx-auto px-4 py-5">
-      <a href="/">
-        <img
-          loading="lazy"
-          srcSet="/logo.png"
-          className="aspect-w-16 aspect-h-10 object-contain w-full max-w-[72px] my-auto"
-          alt="Logo"
-        />
-      </a>
-      <div className="flex space-x-2">
+    <div className="flex flex-col md:flex-row items-center md:justify-between bg-white max-w-screen-md mx-auto px-4 py-5">
+      <div className="flex items-center">
+        <a href="/">
+          <img
+            loading="lazy"
+            srcSet="/logo.png"
+            className="aspect-w-16 aspect-h-10 object-contain w-full max-w-[72px] my-auto"
+            alt="Logo"
+          />
+        </a>
+      
+      
+      <div className={`flex flex-col md:flex-row md:items-center md:space-x-2 ${isMobileMenuOpen ? 'block' : 'hidden'} md:block mt-2 md:mt-0`}>
         <NavLink href="/">Home</NavLink>
         <NavLink href="/about">About</NavLink>
         <NavLink href="/donation">Donation</NavLink>
         <NavLink href="/dashboard">Dashboard</NavLink>
         <NavLink href="/test">Test</NavLink>
-        
       </div>
-
-      <div className="flex items-center gap-2.5">
+      <div className="flex items-center gap-2.5 md:ml-auto">
         <img
-          loading=""
-          src="/s.png"
+          loading="lazy"
+          src="/search.png"
           className="aspect-square object-contain w-12 h-12 overflow-hidden"
           alt="Search Icon"
         />
         <span className="bg-[#D02828] text-white text-sm font-bold leading-5 capitalize whitespace-nowrap px-7 py-4 rounded-full self-start">
           <a href="/login">Log in</a>
         </span>
+        <div className="flex space-x-2 md:hidden ml-auto">
+        <FaBars onClick={toggleMobileMenu} className="cursor-pointer" />
+      </div>
         <div className="flex flex-col items-stretch my-auto gap-2">
           <div className="bg-[#D02828] h-0.5" />
           <div className="bg-[#D02828] h-0.5" />
@@ -58,6 +68,7 @@ function Navbar() {
         </div>
       </div>
     </div>
+  </div>  
   );
 }
 
